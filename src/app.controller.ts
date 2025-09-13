048e89a3d92825f3a6f4c95dd47da50bac7db738
aymensellaouti <aymen.sellaouti@gmail.com>
2025-09-13 18:22:29 +0100

    seanec 1 du 12 9 2025

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World!';
+  }  
+  
+  @Get('cc')
+  getCC(): {message: string} {
+    return {message:'Hello World!'};
   }
 }
